279833a84db6e758e78d260749f07a4cb27d4cd1
nathan-pichon <npichon.walchshofer@gmail.com>
2018-12-03 14:26:43 +0100

    feat(form): handle keyboard input on inputForm

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -4,19 +4,48 @@ import './Form.css';
 class Form extends Component {
     constructor(props) {
         super(props);
+
+        this.searchUser = props.searchUser || (() => {});
         this.state = {
-            users: []
+            users: [],
+            input: ""
         };
+
+        this.setInput = this.setInput.bind(this);
     }
 
-    onSubmit() {
+    setInput(event) {
+        event.preventDefault();
+        this.setState({input: event.target.value});
+        this.searchUser(this.state.input);
+    }
+
+    handleSubmit(event) {
         console.log("submit !");
+        event.preventDefault();
+    }
+
+    handleKeyDown(event) {
+        switch (event.key) {
+            case "Enter":
+                console.log("enter");
+                break;
+            case "ArrowDown":
+            case "ArrowUp":
+                console.log("arrow up or down");
+                event.preventDefault();
+                break;
+            default:
+        }
+        // this.searchUser(this.state.input)
     }
 
     render() {
         return (
             <section>
-                <form onSubmit={this.onSubmit()}>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" value={this.state.input} onChange={this.setInput}
+                           onKeyDown={this.handleKeyDown}/>
                 </form>
             </section>
         );
